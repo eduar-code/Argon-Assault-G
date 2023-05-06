cd3db60350714a16239cd800df961e61fa47aff5
Alexis Eduardo <eduardo.7493z@gmail.com>
2023-05-05 21:12:59 -0600

    Move model and Laser

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -6,13 +6,23 @@ using UnityEngine.InputSystem;
 public class PlayerControls : MonoBehaviour
 {
     [SerializeField] InputAction movement;
+    [SerializeField] float controlSpeed = 20f;
+    [SerializeField] float xRange = 8f;
+    [SerializeField] float yRange = 7f;
+    [SerializeField] float positionPitchFactor = -2f;
+    [SerializeField] float controlPitchFactor = -15f;
+
+    [SerializeField] float positionYawFactor = 2f;
+    [SerializeField] float controlRowFactor = -20f;
+
+    float xThrow, yThrow;
 
     void Start()
     {
 
     }
 
-    void OnEnable()
+    /*void OnEnable()
     {
         movement.Enable();
     }
@@ -20,18 +30,46 @@ public class PlayerControls : MonoBehaviour
     void OnDisable()
     {
         movement.Disable();
-    }
+    }*/
 
     void Update()
     {
+        //float xThrow = movement.ReadValue<Vector2>().x; //new version
+        //float yThrow = movement.ReadValue<Vector2>().y; // new version
+        ProccesTraslation();
+        ProcessRotation();
 
-        float horizontalThrow = movement.ReadValue<Vector2>().x;
-        float verticalThrow = movement.ReadValue<Vector2>().y;
+    }
 
-        //float horizontalThrow = Input.GetAxis("Horizontal");
-        Debug.Log(horizontalThrow); 
+    void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueControlThrow = yThrow * controlPitchFactor;
 
-        //float verticalThrow = Input.GetAxis("Vertical");
-        Debug.Log(verticalThrow); 
+        float pitch = pitchDueToPosition + pitchDueControlThrow;
+        float yaw = transform.localPosition.x * positionYawFactor;
+        float roll =xThrow * controlRowFactor;
+        
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    void ProccesTraslation()
+    {
+        xThrow = Input.GetAxis("Horizontal");
+        yThrow = Input.GetAxis("Vertical");
+
+        float xOffset = xThrow * Time.deltaTime * controlSpeed;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+
+        float yOffset = yThrow * Time.deltaTime * controlSpeed;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(
+            clampedXPos,
+            clampedYPos,
+            transform.localPosition.z
+        );
     }
 }
